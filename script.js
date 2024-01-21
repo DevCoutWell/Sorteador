415a7a29ac854986f573d9a7b2f0dc58292c711b
Wellington Couto <142704831+DevCoutWell@users.noreply.github.com>
2024-01-21 15:24:38 -0300

    script file

---
+++ script.js
@@ -0,0 +1,27 @@
+const listaItens = document.getElementById("ilista");
+const buttonSorter = document.getElementById("buttonSorter");
+
+
+buttonSorter.addEventListener("click", function(){
+
+    if(!listaItens.value){
+        alert("Insira os itens no campo!");
+    }
+    else{
+
+        //transformar em array
+            const arrlista = listaItens.value.split(",");
+        //sortear item
+        const numsorteado = Math.floor(Math.random() * arrlista.length);
+
+        // usar alert para informar qual item foi sorteado
+        const itemSorteado = arrlista[numsorteado];
+        
+        alert(`O item sorteado foi: ${itemSorteado}!`)
+
+
+    }
+
+
+
+});
